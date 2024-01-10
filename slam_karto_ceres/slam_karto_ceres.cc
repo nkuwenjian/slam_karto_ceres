e344bab720cc079e988d4ee65093412dcec1b7ed
nkuwenjian <415664739@qq.com>
2024-01-10 18:21:56 +0800

    Refactor slam karto ceres

--- slam_karto_ceres/slam_karto_ceres.cc
+++ slam_karto_ceres/slam_karto_ceres.cc
@@ -31,95 +31,9 @@
  * Author: Jian Wen (nkuwenjian@gmail.com)
  *****************************************************************************/
 
-#include <map>
-#include <memory>
-#include <mutex>  // NOLINT
-#include <string>
-#include <thread>  // NOLINT
-#include <vector>
-
-#include "message_filters/subscriber.h"
-#include "nav_msgs/GetMap.h"
-#include "nav_msgs/MapMetaData.h"
-#include "open_karto/Mapper.h"
-#include "ros/console.h"
-#include "ros/ros.h"
-#include "sensor_msgs/LaserScan.h"
-#include "tf/message_filter.h"
-#include "tf/transform_broadcaster.h"
-#include "tf/transform_listener.h"
-
-#include "slam_karto_ceres/ceres_solver.h"
-
-#define MAP_IDX(sx, i, j) ((sx) * (j) + (i))
-
-#define UNUSED(var) (void)(var)
+#include "slam_karto_ceres/slam_karto_ceres.h"
 
 namespace slam_karto_ceres {
-
-class SlamKartoCeres {
- public:
-  SlamKartoCeres() = default;
-  virtual ~SlamKartoCeres();
-  void Initialize();
-
- private:
-  void LaserCallback(const sensor_msgs::LaserScan::ConstPtr& scan);
-  bool MapCallback(nav_msgs::GetMap::Request& req,    // NOLINT
-                   nav_msgs::GetMap::Response& res);  // NOLINT
-  void LoadRosParamFromNodeHandle(const ros::NodeHandle& nh,
-                                  double* transform_publish_period);
-  bool GetOdomPose(karto::Pose2* karto_pose, const ros::Time& t);
-  const karto::LaserRangeFinder* GetLaser(
-      const sensor_msgs::LaserScan::ConstPtr& scan);
-  bool AddScan(const karto::LaserRangeFinder* laser,
-               const sensor_msgs::LaserScan::ConstPtr& scan,
-               karto::Pose2* karto_pose);
-  bool UpdateMap();
-  void PublishTransform();
-  void PublishLoop(double transform_publish_period);
-
-  ros::NodeHandle nh_;
-  tf::TransformListener tf_;
-  std::unique_ptr<tf::TransformBroadcaster> tf_broadcaster_ = nullptr;
-  std::unique_ptr<message_filters::Subscriber<sensor_msgs::LaserScan>>
-      scan_filter_sub_ = nullptr;
-  std::unique_ptr<tf::MessageFilter<sensor_msgs::LaserScan>> scan_filter_ =
-      nullptr;
-  ros::Publisher sst_;
-  ros::Publisher sstm_;
-  ros::ServiceServer ss_;
-  nav_msgs::GetMap::Response map_;
-
-  // Storage for ROS parameters.
-  std::string map_frame_;
-  std::string odom_frame_;
-  std::string base_frame_;
-  double map_update_interval_;
-
-  // Range threshold of laser range finder.
-  double range_threshold_;
-
-  // Time for tolerance on the published transform, basically defines how long a
-  // map->odom transform is good for.
-  double transform_tolerance_;
-
-  double resolution_;
-  std::mutex map_mutex_;
-  std::mutex map_to_odom_mutex_;
-
-  // Karto bookkeeping.
-  std::unique_ptr<karto::Mapper> mapper_ = nullptr;
-  std::unique_ptr<karto::Dataset> dataset_ = nullptr;
-  std::unique_ptr<CeresSolver> solver_ = nullptr;
-  std::map<std::string, const karto::LaserRangeFinder*> lasers_;
-
-  // Internal state.
-  bool got_map_ = false;
-  std::unique_ptr<std::thread> transform_thread_ = nullptr;
-  tf::Transform map_to_odom_;
-  std::chrono::system_clock::time_point last_map_update_timestamp_;
-};
 
 void SlamKartoCeres::Initialize() {
   map_to_odom_.setIdentity();
@@ -632,16 +546,3 @@ bool SlamKartoCeres::MapCallback(nav_msgs::GetMap::Request& req,
 }
 
 }  // namespace slam_karto_ceres
-
-int main(int argc, char* argv[]) {
-  google::InitGoogleLogging(argv[0]);
-  FLAGS_logtostderr = true;
-  ros::init(argc, argv, "slam_karto_ceres");
-
-  slam_karto_ceres::SlamKartoCeres slam_karto_ceres;
-  slam_karto_ceres.Initialize();
-
-  ros::spin();
-
-  return 0;
-}
