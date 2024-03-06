917211fecc20c1812b73dcdda4245368f709e8ba
nkuwenjian <415664739@qq.com>
2024-03-06 19:54:22 +0800

    Add slam_karto_ceres_test

--- test/slam_karto_ceres_test.cc
+++ test/slam_karto_ceres_test.cc
@@ -1,19 +1,35 @@
-/*
- * Copyright 2010 SRI International
+/******************************************************************************
+ * Copyright (c) 2023, NKU Mobile & Flying Robotics Lab
+ * All rights reserved.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Jian Wen (nkuwenjian@gmail.com)
+ *****************************************************************************/
 
 #include <memory>
 
@@ -21,6 +37,8 @@
 #include "open_karto/Mapper.h"
 
 #include "slam_karto_ceres/ceres_solver.h"
+
+#define UNUSED(var) (void)(var)
 
 namespace slam_karto_ceres {
 
@@ -56,8 +74,7 @@ std::unique_ptr<karto::Dataset> CreateMap(karto::Mapper* mapper) {
 
   {
     // Create localized range scan.
-    karto::LocalizedRangeScan* range_scan =
-        new karto::LocalizedRangeScan(name, readings);
+    auto* range_scan = new karto::LocalizedRangeScan(name, readings);
     range_scan->SetOdometricPose({0.0, 0.0, 0.0});
     range_scan->SetCorrectedPose({0.0, 0.0, 0.0});
 
@@ -73,8 +90,7 @@ std::unique_ptr<karto::Dataset> CreateMap(karto::Mapper* mapper) {
 
   {
     // Create localized range scan.
-    karto::LocalizedRangeScan* range_scan =
-        new karto::LocalizedRangeScan(name, readings);
+    auto* range_scan = new karto::LocalizedRangeScan(name, readings);
     range_scan->SetOdometricPose({1.0, 0.0, 1.57});
     range_scan->SetCorrectedPose({1.0, 0.0, 1.57});
 
@@ -90,8 +106,7 @@ std::unique_ptr<karto::Dataset> CreateMap(karto::Mapper* mapper) {
 
   {
     // Create localized range scan.
-    karto::LocalizedRangeScan* range_scan =
-        new karto::LocalizedRangeScan(name, readings);
+    auto* range_scan = new karto::LocalizedRangeScan(name, readings);
     range_scan->SetOdometricPose({1.0, -1.0, 2.35619449});
     range_scan->SetCorrectedPose({1.0, -1.0, 2.35619449});
 
@@ -170,6 +185,7 @@ void PrintOccupancyGrid(const karto::OccupancyGrid* occ_grid) {
 }  // namespace slam_karto_ceres
 
 int main(int argc, char* argv[]) {
+  UNUSED(argc);
   google::InitGoogleLogging(argv[0]);
   FLAGS_logtostderr = true;
 
